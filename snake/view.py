4faff3abf255a539445d83c1bc9ef1cc19c6046c
SymAir <symeon@oclocher.fr>
2025-06-29 16:44:37 +0200

    Tweaked some paramters

--- snake/view.py
+++ snake/view.py
@@ -9,8 +9,8 @@ warnings.filterwarnings("ignore", category=UserWarning, module="pygame.pkgdata")
 
 env = SnakeEnv(render_mode="human")
 
-models_dir = "models/PPO-207202"
-model_path = f"{models_dir}/1000000.zip"
+models_dir = "models/PPO-206711"
+model_path = f"{models_dir}/3500000.zip"
 model = PPO.load(model_path, env=env)
 
 print(f"Displaying {model_path}")
